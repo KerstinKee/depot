a52444758a677baa313b1b17714cb6da18f7624b
Kee Ruby <eichmann@fidor.de>
2014-10-04 18:21:26 +0200

    additional Controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -61,7 +61,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
