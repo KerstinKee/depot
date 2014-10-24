68701a2c0fc99573f9f7f6abe5a9ef38e8d48496
Kee Ruby <eichmann@fidor.de>
2014-10-24 23:58:00 +0200

    Clone in Desktop

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,7 +6,7 @@ class Cart < ActiveRecord::Base
   def add_product(product_id)
     current_item = line_items.find_by(product_id: product_id)
     if current_item
-     current_item.quantity = current_item.quantity.blank? ? 1 : current_item.quantity + 1
+     current_item.quantity = current_item.quantity.to_i + 1
     else
      current_item = line_items.build(product_id: product_id)
    end
