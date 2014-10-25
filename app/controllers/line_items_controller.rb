5e20922f7fcbb373e089b159b7b43cc60b37a9a1
Kee Ruby <eichmann@fidor.de>
2014-10-25 11:17:56 +0200

    rake db Drop create migrate

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -46,7 +46,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.update(line_item_params)
         format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { render :show, st‚atus: :ok, location: @line_item }
+        format.json { render :show, status: :ok, location: @line_item }
       else
         format.html { render :edit }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
